5571393ef710fbe4fe63a1a0550d61bce7bc5927
jayeshsojitra103 <jayesh.sojitra103@gmail.com>
2024-10-02 10:51:14 +0530

    Step 3: Add support for single and multiple comma-separated numbers

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,6 +1,7 @@
 export default function add(numbers) {
-    if (numbers === "") {
-      return 0;
-    }
-    return null;
+    if (numbers === "") return 0;
+
+    const numberArray = numbers.split(/,|\n/).map(Number);
+    console.log("numberArray",numberArray)
+    return numberArray.reduce((sum, num) => sum + num, 0);
   }
--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -3,3 +3,11 @@ import add from './StringCalculator';
 test('should return 0 for an empty string', () => {
   expect(add("")).toBe(0);
 });
+
+test('should return the number itself if one number is provided', () => {
+    expect(add("1")).toBe(1);
+  });
+  
+  test('should return the sum of two numbers', () => {
+    expect(add("1,5")).toBe(6);
+  });
