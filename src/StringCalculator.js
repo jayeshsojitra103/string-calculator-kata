b6cff810642ef9e53b71ae82d13e3c027c467224
jayeshsojitra103 <jayesh.sojitra103@gmail.com>
2024-10-02 11:08:12 +0530

    Step 4: Add support for new lines between numbers

--- src/StringCalculator.js
+++ src/StringCalculator.js
@@ -1,7 +1,6 @@
 export default function add(numbers) {
-    if (numbers === "") return 0;
+  if (numbers === "") return 0;
 
-    const numberArray = numbers.split(/,|\n/).map(Number);
-    console.log("numberArray",numberArray)
-    return numberArray.reduce((sum, num) => sum + num, 0);
-  }
+  const numberArray = numbers.split(/,|\n/).map(Number);
+  return numberArray.reduce((sum, num) => sum + num, 0);
+}
--- src/StringCalculator.test.js
+++ src/StringCalculator.test.js
@@ -1,13 +1,19 @@
-import add from './StringCalculator';
+import add from "./StringCalculator";
 
-test('should return 0 for an empty string', () => {
-  expect(add("")).toBe(0);
-});
+describe("String Calculator Tests", () => {
+  test("should return 0 for an empty string", () => {
+    expect(add("")).toBe(0);
+  });
 
-test('should return the number itself if one number is provided', () => {
+  test("should return the number itself if one number is provided", () => {
     expect(add("1")).toBe(1);
   });
-  
-  test('should return the sum of two numbers', () => {
+
+  test("should return the sum of two numbers", () => {
     expect(add("1,5")).toBe(6);
   });
+
+  test("should handle new lines between numbers", () => {
+    expect(add("1\n2,3")).toBe(6);
+  });
+});
